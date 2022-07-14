8c57acce3e56698dbd6cf9895c3122964827c43d
BParadowski <bartoszparadowski01@gmail.com>
2022-07-14 17:44:31 +0200

    Add drawing and new grid functionalities

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const square = document.createElement('div');
 const squareContainer = document.getElementById("squares");
-let xbyx = 4;
+const squareNumbers = document.getElementById("numOfSquares");
+squareNumbers.textContent = "16x16";
+const button = document.getElementById("newGrid");
+let xbyx = 16;
+drawGrid(xbyx);
+
+const squares = document.querySelectorAll('#row div');
+squares.forEach(square => square.addEventListener('mouseover', paint));
+button.addEventListener('click', backToWhite);
 
 function drawGrid(x){
     let j = 0;
@@ -17,5 +24,14 @@ function drawGrid(x){
     squareContainer.appendChild(square);
     j++;
     }
+    squareNumbers.textContent = `${x}x${x}`;
 }
-drawGrid(xbyx);
+
+function paint(e){
+    this.classList.add("colored");
+}
+
+function backToWhite(){
+    squares.forEach(square => square.classList.remove("colored"));
+}
+
